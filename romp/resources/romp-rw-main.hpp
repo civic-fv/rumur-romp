5baa717106bf846b2161394091e07ca3d173d350
ajanthav10 <ajanthav10@gmail.com>
2022-05-25 08:36:43 -0600

    added

--- romp/resources/romp-rw-main.hpp
+++ romp/resources/romp-rw-main.hpp
@@ -30,9 +30,66 @@
  * @param args 
  * @return \c int - 
  */
-int main(char* argv, int args)
+/**
+ * @proj romp
+ * @author Andrew Osterhout
+ * @author Ajantha Varadharaaj
+ * @org University of Utah (UofU) School of Computing (SoC)
+ * @org Center for Parallel compute at Utah (CPU)
+ * @org <a href="https://civic-fv.github.io">Civic-fv NSF Grant</a>
+ * @org Ganesh Gopalakrishnan's Research Group
+ * @file romp-main.hpp
+ * 
+ * @brief Just where the main function is.
+ *        If anything else exists in here, it should only be called in here and nowhere else.
+ * 
+ * @date 2022/05/11
+ * @version 0.1
+ */
+
+#include <string.h>
+
+#ifdef __romp__GENERATED_CODE
+#include "romp-rw.hpp"
+#include "romp-rse.hpp"
+#endif
+
+
+
+/**
+ * @brief 
+ * @param argv 
+ * @param args 
+ * @return \c int - 
+ */
+void setup_infile(char *filename)
 {
-  //TODO: the CLI stuff and launching the main romp function.
+  /* pre-checking on filename. */
+  int len = strlen(filename);
+  if (filename[len - 2] != '.' || filename[len - 1] != 'm') {
+    Error.FatalError("Murphi only handles files ending in \".m\"");
+  } else {
+    gFileName = filename;
+    yyin = fopen(filename, "r");	// yyin is flex's global for the input file
+    if (yyin == NULL) {
+      Error.FatalError("%s:No such file or directory.", filename);
+    }
+  }
+}
+int main(int argc, char *argv[])
+{
+  int error;
+  args = new argclass(argc, argv);
+  init_globals();//initialize all global variables to be done 
+  error = yyparse();
+  
 }
 
 
+
+
+
+
+
+
+
--- romp/resources/romp-rw-options.hpp
+++ romp/resources/romp-rw-options.hpp
@@ -13,3 +13,73 @@
  * @date 2022/05/18
  * @version 0.1
  */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <float.h>
+#include <math.h>
+
+#define __FILE_defined
+struct _IO_FILE;
+typedef struct _IO_FILE FILE;
+
+
+/* Extern Variables */
+
+extern argclass *args;
+extern char *gFileName;		/* file support. */
+extern FILE *codefile;
+
+extern int yyparse(void);	
+class argclass 
+{
+  int argc;
+  char **argv;
+public:
+  // variables
+  
+
+  // initializer
+   argclass(int ac, char **av);
+
+  // supporting routines
+  void PrintOptions(void);
+}
+
+
+argclass::argclass(int ac, char **av)
+:
+argc(ac), argv(av), print_license(FALSE), help(FALSE)
+{
+  bool initialized_filename = FALSE;
+  int i;
+
+  if (ac == 1) {		// if there is no argument, print help
+    help = TRUE;
+    PrintInfo();
+    exit(1);
+  }
+
+  if (strcmp(av[i], "-h") == 0) {
+      help = TRUE;
+      PrintInfo();
+      exit(1);
+    }
+
+void argclass::PrintInfo(void)
+{
+  
+  if (!help)
+    printf("Run this program with \"-h\" for the list of options.\n");
+
+  if (help)
+    PrintOptions();
+  fflush(stdout);
+}
+
+void argclass::PrintOptions(void)
+{
+  printf("The options are shown as follows:\n\
+----- to be filled------------");
+}
