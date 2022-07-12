66923fba9abdb24a835ddaba474fdf555fdbd644
Ajantha varadharaaj <ajanthav10@gmail.com>
2022-07-12 11:40:44 -0600

    added

--- romp/resources/romp-rw-cli.hpp
+++ romp/resources/romp-rw-cli.hpp
@@ -29,6 +29,7 @@ void print_help()
               parameter  or  pass  0, the number of threads will be chosen based on the available
               hardware threads on the platform on which you generate the model.\n\
 \t-w    \t\t   \tnumber of random walkers to be launched by the verifier\n\
+\t-n    \t\t   \tno deadlock detection\n\
 \t-s    \t\t   \trandom seed to be fed into the random walker. If not provided, default seed is considered.\n");
 }
 static void parse_args(int argc, char **argv) {
@@ -36,16 +37,13 @@ static void parse_args(int argc, char **argv) {
   for (;;) // as an infinite loop consider changing to finite loop with argc as check ??
     enum {
       OPT_DEPTH = 100,
-      OPT_NO_DEADLOCK_DETECTION,
+      //OPT_NO_DEADLOCK_DETECTION,
       OPT_TRACE,
-      OPT_THREAD,
-      OPT_RANDOMWALKERS,
-      OPT_RANDOMSEED
     };
 
     static struct option opts[] = {
-        {"depth", required_argument, 0, OPT_}, // whats the point of 0 in here ?
-        {"nodeadlock-detection", required_argument, 0, 'd'}, // not required of args right ??
+        {"depth", required_argument, 0, 'd'}, // whats the point of 0 in here ?
+        {"nodeadlock-detection", required_argument, 0, 'ndl'}, // not required of args right ??
         {"help", no_argument, 0, 'h'},
         {"threads", required_argument, 0, 't'},
         {"rand_walker", required_argument, 0, 'w'},
@@ -55,7 +53,8 @@ static void parse_args(int argc, char **argv) {
     };
 
 int option_index = 0;
-int c = getopt_long(argc, argv, "hdt:w:s:", opts, &option_index);
+int c = getopt_long(argc, argv, "hndlt:w:s:", opts, &option_index); // compund letters args ?? also not for trace//depth ??
+extern char *optarg;		
 
     if (c == -1)
       break;
@@ -65,12 +64,18 @@ int c = getopt_long(argc, argv, "hdt:w:s:", opts, &option_index);
     case 'h': // --help
       print_help();
       exit(EXIT_SUCCESS);
+      break;
+
+    case 'ndl': // --no-deadlock-detection ...
+      //define ndl;
+      exit(EXIT_FAILURE);
+      break;
 
     case 't': { // --threads ...
       bool valid = true;
       try {
         options.threads = optarg;
-        if (options.threads < 0)
+        if (options.threads < 0) //any other cond to be added ??
           valid = false;
       } catch (std::invalid_argument &) {
         valid = false;
@@ -81,94 +86,68 @@ int c = getopt_long(argc, argv, "hdt:w:s:", opts, &option_index);
       }
       break;
     }
-case 't': { // --threads ...
+    case 'w': { // random walkers to be launched
       bool valid = true;
       try {
-        options.threads = optarg;
-        if (options.threads < 0)
+        options.random_walkers = optarg;
+        if (options.random_walkers < 0) // any other cond for rand walkers ??
           valid = false;
       } catch (std::invalid_argument &) {
         valid = false;
       }
       if (!valid) {
-        std::cerr << "invalid --threads argument \"" << optarg << "\"\n";
+        std::cerr << "invalid --random walkers argument \"" << optarg << "\"\n";
         exit(EXIT_FAILURE);
       }
       break;
     }
-case 't': { // --threads ...
+    case 's': { //  random seed for the random walkers 
       bool valid = true;
       try {
-        options.threads = optarg;
-        if (options.threads < 0)
+        options.rand_seed = optarg;
+        if (options.rand_seed < 0)  // any other cond for rand seed 
           valid = false;
       } catch (std::invalid_argument &) {
         valid = false;
       }
       if (!valid) {
-        std::cerr << "invalid --threads argument \"" << optarg << "\"\n";
+        std::cerr << "invalid --random seed argument \"" << optarg << "\"\n";
         exit(EXIT_FAILURE);
       }
       break;
     }
-    case 'v': // --verbose
-      options.log_level = LogLevel::INFO;
-      set_log_level(options.log_level);
-      break;
 
     case '?':
       std::cerr << "run `" << argv[0] << " --help` to see available options\n";
       exit(EXIT_FAILURE);
 
     case OPT_TRACE: // --trace ...
-      if (strcmp(optarg, "handle_reads") == 0) {
-        options.traces |= TC_HANDLE_READS;
-      } else if (strcmp(optarg, "handle_writes") == 0) {
-        options.traces |= TC_HANDLE_WRITES;
-      } else if (strcmp(optarg, "memory_usage") == 0) {
-        options.traces |= TC_MEMORY_USAGE;
-      } else if (strcmp(optarg, "queue") == 0) {
-        options.traces |= TC_QUEUE;
-      } else if (strcmp(optarg, "set") == 0) {
-        options.traces |= TC_SET;
-      } else if (strcmp(optarg, "symmetry_reduction") == 0) {
-        options.traces |= TC_SYMMETRY_REDUCTION;
-      } else if (strcmp(optarg, "all") == 0) {
-        options.traces = uint64_t(-1);
-      } else {
+      if (strcmp(optarg, "verbose") == 0) {
+        options.traces |= TC_VERBOSE;
+      } else if (strcmp(optarg, "tripped_state") == 0) {
+        options.traces |= TC_TRIPPED_STATES;
+      } else if (strcmp(optarg, "rw_path") == 0) {
+        options.traces |= TC_RW_PATH;
+      } 
+      else {
         std::cerr << "invalid --trace argument \"" << optarg << "\"\n"
-                  << "valid arguments are \"handle_reads\", \"handle_writes\", "
-                     "\"memory_usage\", \"queue\", \"set\", and "
-                     "\"symmetry_reduction\"\n";
+                  << "valid arguments are \"verbose\", \"tripped_state\", "
+                     "\"rw_path\",\n";
         exit(EXIT_FAILURE);
       }
       break;
 
-    case OPT_DEADLOCK_DETECTION: // --deadlock-detection ...
-      if (strcmp(optarg, "off") == 0) {
-        options.deadlock_detection = DeadlockDetection::OFF;
-      } else if (strcmp(optarg, "stuck") == 0) {
-        options.deadlock_detection = DeadlockDetection::STUCK;
-      } else if (strcmp(optarg, "stuttering") == 0) {
-        options.deadlock_detection = DeadlockDetection::STUTTERING;
-      } else {
-        std::cerr << "invalid argument to --deadlock-detection, \"" << optarg
-                  << "\"\n";
-        exit(EXIT_FAILURE);
-      }
-      break;
-
-    case OPT_BOUND: { // --bound ...
+    case OPT_DEPTH: { // --depth ...
       bool valid = true;
       try {
-        options.bound = optarg;
-        if (options.bound < 0)
+        options.depth = optarg;
+        if (options.depth < 0 && >= INT32_MAX)
           valid = false;
       } catch (std::invalid_argument &) {
         valid = false;
       }
       if (!valid) {
-        std::cerr << "invalid --bound argument \"" << optarg << "\"\n";
+        std::cerr << "invalid --depth argument \"" << optarg << "\"\n";
         exit(EXIT_FAILURE);
       }
       break;
@@ -187,4 +166,7 @@ int main(int argc, char **argv) {
    */
   parse_args(argc, argv);
 
- 
+ // Parse input model
+  // TODO shit to figure out ??
+  return EXIT_SUCCESS;
+}
